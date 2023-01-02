86261a353a0bb66298c23703f69c66bb7359e38d
Tomer <iwantomer@hotmail.com>
2023-01-02 22:13:49 +0100

    test: load vusaverse packages

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+.onLoad <- function(libname, pkgname) {
+  install_github("vusaverse/vvauditor")
+  install_github("vusaverse/vvcommander")
+  install_github("vusaverse/vvconverter")
+  install_github("vusaverse/vvfiller")
+  install_github("vusaverse/vvmodeller")
+  install_github("vusaverse/vvmover")
+  install_github("vusaverse/vvsculptor")
+}
